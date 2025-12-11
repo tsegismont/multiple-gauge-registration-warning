63a7267c74b6617930efd5476aea57acc63de2db
Thomas Segismont <tsegismo@ibm.com>
2025-12-11 17:33:43 +0100

    Add more info

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,33 +1,64 @@
 package org.example;
 
+import io.micrometer.core.instrument.config.MeterFilter;
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 import org.example.micrometer.ActiveTaskMetrics;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
 
   public static void main(String[] args) {
     SimpleMeterRegistry registry = new SimpleMeterRegistry();
-    //registry.config().meterFilter(MeterFilter.ignoreTags("address"));
+    registry.config().meterFilter(MeterFilter.ignoreTags("address"));
 
     // MessageBus bus = new MessageBus(new MicrometerMetrics(new LongGauges(new ConcurrentHashMap<>()), registry));
     MessageBus bus = new MessageBus(new ActiveTaskMetrics(registry));
 
-    bus.registerConsumer("foo", s -> {
-      System.out.println(registry.getMetersAsString());
-      System.out.printf("foo: %s%n", s);
-    });
-    bus.registerConsumer("bar", s -> {
-      System.out.println(registry.getMetersAsString());
-      System.out.printf("bar: %s%n", s);
-    });
+    try (ExecutorService executor = Executors.newCachedThreadPool()) {
 
-    bus.deliverMessage("foo", "Hello");
-    bus.deliverMessage("foo", "Hello");
-    bus.deliverMessage("foo", "Hello");
+      CountDownLatch latch = new CountDownLatch(1);
 
-    bus.deliverMessage("bar", "Hi");
-    bus.deliverMessage("bar", "Hi");
+      bus.registerConsumer("foo", s -> {
+        System.out.printf("foo: %s%n", s);
+        try {
+          latch.await();
+        } catch (InterruptedException e) {
+          throw new RuntimeException(e);
+        }
+      });
+      bus.registerConsumer("bar", s -> {
+        System.out.printf("bar: %s%n", s);
+        try {
+          latch.await();
+        } catch (InterruptedException e) {
+          throw new RuntimeException(e);
+        }
+      });
 
-    System.out.println(registry.getMetersAsString());
+      executor.submit(() -> {
+        bus.deliverMessage("foo", "Hello");
+      });
+      executor.submit(() -> {
+        bus.deliverMessage("foo", "Hello");
+      });
+      executor.submit(() -> {
+        bus.deliverMessage("foo", "Hello");
+      });
+      executor.submit(() -> {
+        bus.deliverMessage("bar", "Hi");
+      });
+      executor.submit(() -> {
+        bus.deliverMessage("bar", "Hi");
+      });
+
+      latch.countDown();
+      Thread.sleep(2000);
+
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
--- src/main/java/org/example/micrometer/ActiveTaskMetrics.java
+++ src/main/java/org/example/micrometer/ActiveTaskMetrics.java
@@ -1,20 +1,22 @@
 package org.example.micrometer;
 
-import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.MultiGauge;
 import io.micrometer.core.instrument.Tags;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 import org.example.MetricsSPI;
 import org.example.micrometer.meters.MultiGaugeUpdater;
 
 public class ActiveTaskMetrics implements MetricsSPI {
 
+  private final SimpleMeterRegistry registry;
   private final MultiGaugeUpdater multiGaugeUpdater;
 
-  public ActiveTaskMetrics(MeterRegistry registry) {
+  public ActiveTaskMetrics(SimpleMeterRegistry registry) {
     this(registry, Tags.empty());
   }
 
-  public ActiveTaskMetrics(MeterRegistry registry, Tags commonTags) {
+  public ActiveTaskMetrics(SimpleMeterRegistry registry, Tags commonTags) {
+    this.registry = registry;
     MultiGauge multiGauge = MultiGauge.builder("messagePending")
       .description("Number of active messages")
       .tags(commonTags)
@@ -26,10 +28,12 @@ public class ActiveTaskMetrics implements MetricsSPI {
   @Override
   public void messageReceived(String address) {
     multiGaugeUpdater.increment(Tags.of("address", address));
+    System.out.printf("ActiveTaskMetrics.messageReceived: %n==%n%s%n==%n", registry.getMetersAsString());
   }
 
   @Override
   public void messageProcessed(String address) {
     multiGaugeUpdater.decrement(Tags.of("address", address));
+    System.out.printf("ActiveTaskMetrics.messageProcessed: %n==%n%s%n==%n", registry.getMetersAsString());
   }
 }
